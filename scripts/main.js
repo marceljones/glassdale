c97e892df8944dd5e3fb3607930ff8cc6a13056f
marceljones <marceljones.mj@gmail.com>
2020-09-03 15:04:45 -0400

    still working on notes

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,24 @@ import {OfficerList} from './officers/OfficerList.js';
 import {CriminalList} from './criminals/CriminalList.js';
 import {ConvictionSelect} from './convictions/ConvictionSelect.js';
 import { OfficerSelect } from "./officers/OfficerSelect.js";
-// import { NoteForm } from './criminals/notes/NoteForm.js';
+import { NoteForm } from './notes/NoteForm.js';
 OfficerList();
 CriminalList();
 ConvictionSelect();
 OfficerSelect();
+NoteForm();
+
+  
+// console.log("Good morning, Main.js");
+
+// import {OfficerList} from './officers/OfficerList.js';
+// import {CriminalList} from './criminals/CriminalList.js';
+// import{ConvictionSelect} from './convictions/ConvictionSelect.js';
+// import { OfficerSelect } from './officers/OfficerSelect.js';
+// import {NoteForm} from './notes/NoteForm.js'
+
+// // OfficerList();
+// CriminalList();
+// ConvictionSelect();	
+// OfficerSelect();
 // NoteForm();
